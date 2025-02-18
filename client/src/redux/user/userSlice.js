eff76a0f4ed0acebbd52541bcbd4cd0f23d10bd3
Praveenegi <130302555+Praveenegi@users.noreply.github.com>
2025-02-18 22:21:19 +0530

    upadte code and remove two headerfile from main.jsx and userSlice.js

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,5 +1,4 @@
-import { createSlice, current } from "@reduxjs/toolkit";
-import e from "express";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { 
     currentUser: null,
