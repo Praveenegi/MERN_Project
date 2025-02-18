0b377cf2c0c2917a07b480fdf85efa7774a3a817
Praveenegi <130302555+Praveenegi@users.noreply.github.com>
2025-02-18 20:26:56 +0530

    add redux toolkit

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -65,7 +65,7 @@ export default function SignUp() {
       </form>
       <div className="flex gap-2 mt-2">
         <p>Have an account?</p> 
-        <Link to = {"/sign-in"}>
+        <Link to = {"/signin"}>
         <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
